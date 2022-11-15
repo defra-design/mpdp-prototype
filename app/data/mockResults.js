82cc57a8e533f26f3533ecf44a8abc3c42830a04
Mikael Metthey <53611716+mikaelmetthey@users.noreply.github.com>
2022-11-15 16:06:20 +0000

    Update on data and search

--- app/data/mockResults.js
+++ app/data/mockResults.js
@@ -1,157 +1,157 @@
 module.exports = [
   {
-    payee_name: 'T R Carter & Sons 1',
+    payee_name: 'Alex Carter',
     part_postcode: 'RG1',
     town: 'Reading',
     county_council: 'Berkshire',
-    amount: '£11,000000'
+    amount: '£1,000'
   },
   {
-    payee_name: 'T R Carter & Sons 2',
+    payee_name: 'Richard Carter',
     part_postcode: 'RG1',
     town: 'Reading',
     county_council: 'Berkshire',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
-    payee_name: 'T R Carter & Sons 3',
+    payee_name: 'Helen Carter',
     part_postcode: 'RG1',
     town: 'Reading',
     county_council: 'Berkshire',
-    amount: '£10,200'
+    amount: '£1,000'
   },
   {
-    payee_name: 'T R Carter & Sons 4',
+    payee_name: 'Alistair Carter',
     part_postcode: 'RG1',
     town: 'Reading',
     county_council: 'Berkshire',
-    amount: '£10,743.55'
+    amount: '£1,000'
   },
   {
-    payee_name: 'T R Carter & Sons 5',
+    payee_name: 'Alec Carter',
     part_postcode: 'RG1',
     town: 'Reading',
     county_council: 'Berkshire',
-    amount: '£11,000'
+    amount: '£1,000'
   },
   {
-    payee_name: 'T R Carter & Sons 6',
+    payee_name: 'Laura Carter',
     part_postcode: 'SL4',
     town: 'Winkfield',
     county_council: 'Berkshire',
-    amount: '£50,000'
+    amount: '£1,000'
   },
   {
-    payee_name: 'T R Carter & Sons 7',
+    payee_name: 'Mary Carter',
     part_postcode: 'SL4',
     town: 'Winkfield',
     county_council: 'Berkshire',
-    amount: '£2,040'
+    amount: '£1,000'
   },
   {
-    payee_name: 'T R Carter & Sons 8',
+    payee_name: 'John Carter',
     part_postcode: 'SL4',
     town: 'Winkfield',
     county_council: 'Berkshire',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
-    payee_name: 'T R Carter & Sons 9',
+    payee_name: ' Carter',
     part_postcode: 'SL4',
     town: 'Winkfield',
     county_council: 'Berkshire',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 10',
     part_postcode: 'SL4',
     town: 'Winkfield',
     county_council: 'Berkshire',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 11',
     part_postcode: 'LS6',
     town: 'Leeds',
     county_council: 'Yorkshire and The Humber',
-    amount: '£20,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 12',
     part_postcode: 'LS6',
     town: 'Leeds',
     county_council: 'Yorkshire and The Humber',
-    amount: '£30,500'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 13',
     part_postcode: 'LS6',
     town: 'Leeds',
     county_council: 'Yorkshire and The Humber',
-    amount: '£12,330'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 14',
     part_postcode: 'LS6',
     town: 'Leeds',
     county_council: 'Yorkshire and The Humber',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 15',
     part_postcode: 'LS6',
     town: 'Leeds',
     county_council: 'Yorkshire and The Humber',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 16',
     part_postcode: 'LS6',
     town: 'Leeds',
     county_council: 'Yorkshire and The Humber',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 17',
     part_postcode: 'TS24',
     town: 'Hartlepool',
     county_council: 'Durham',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 18',
     part_postcode: 'TS24',
     town: 'Hartlepool',
     county_council: 'Durham',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 19',
     part_postcode: 'TS24',
     town: 'Hartlepool',
     county_council: 'Durham',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 20',
     part_postcode: 'TS24',
     town: 'Hartlepool',
     county_council: 'Durham',
-    amount: '£10,000'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 21',
     part_postcode: 'TS24',
     town: 'Hartlepool',
     county_council: 'Durham',
-    amount: '£11,200'
+    amount: '£1,000'
   },
   {
     payee_name: 'T R Carter & Sons 22',
     part_postcode: 'TS24',
     town: 'Hartlepool',
     county_council: 'Durham',
-    amount: '£122,300.25'
+    amount: '£1,000'
   }
 
 ]
