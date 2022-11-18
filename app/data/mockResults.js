8053b07228bf0c213614bca1605a30916646dab0
Mikael Metthey <53611716+mikaelmetthey@users.noreply.github.com>
2022-11-18 11:28:25 +0000

    Modifying the amounts to be over 1250

--- app/data/mockResults.js
+++ app/data/mockResults.js
@@ -4,217 +4,217 @@ module.exports = [
     town: 'Abingdon',
     county_council: 'Oxfordshire',
     part_postcode: 'OX14',
-    amount: '£1,000'
+    amount: '£5,000'
   },
   {
     payee_name: 'Carter Ltd',
     town: 'Adderbury',
     county_council: 'Oxfordshire',
     part_postcode: 'OX17',
-    amount: '£1,000'
+    amount: '£10,500'
   },
   {
     payee_name: 'Carter Wells Ltd',
     town: 'Adwell',
     county_council: 'Oxfordshire',
     part_postcode: 'OX9',
-    amount: '£1,000'
+    amount: '£7,250'
   },
   {
     payee_name: 'Carter Brothers',
     town: 'Alkerton',
     county_council: 'Oxfordshire',
     part_postcode: 'OX15',
-    amount: '£1,000'
+    amount: '£870'
   },
   {
     payee_name: 'Carter Village Farm',
     town: 'Alvescot',
     county_council: 'Oxfordshire',
     part_postcode: 'OX18',
-    amount: '£1,000'
+    amount: '£12,340'
   },
   {
     payee_name: 'Caterham Foods Ltd',
     town: 'Ambrosden',
     county_council: 'Oxfordshire',
     part_postcode: 'OX6',
-    amount: '£1,000'
+    amount: '£1,750'
   },
   {
     payee_name: 'Carter-Boxhall Vale',
     town: 'Appleford',
     county_council: 'Oxfordshire',
     part_postcode: 'OX14',
-    amount: '£1,000'
+    amount: '£9,650'
   },
   {
     payee_name: 'Cartswell Farm',
     town: 'Appleton',
     county_council: 'Oxfordshire',
     part_postcode: 'OX13',
-    amount: '£1,000'
+    amount: '£21,500'
   },
   {
     payee_name: 'Cartworth Partners',
     town: 'Ardington',
     county_council: 'Oxfordshire',
     part_postcode: 'OX12',
-    amount: '£1,000'
+    amount: '£2,170'
   },
   {
     payee_name: 'Cartwright Dairy',
     town: 'Ardley',
     county_council: 'Oxfordshire',
     part_postcode: 'OX6',
-    amount: '£1,000'
+    amount: '£13,500'
   },
   {
     payee_name: 'Carling Vale Ltd',
     town: 'Ascott-Under-Wychwood',
     county_council: 'Oxfordshire',
     part_postcode: 'OX7',
-    amount: '£1,000'
+    amount: '£5,700'
   },
   {
     payee_name: 'Carford Valley Limited',
     town: 'Asthall',
     county_council: 'Oxfordshire',
     part_postcode: 'OX18',
-    amount: '£1,000'
+    amount: '£3,050'
   },
   {
     payee_name: 'Cartington grains',
     town: 'Asthall Leigh',
     county_council: 'Oxfordshire',
     part_postcode: 'OX8',
-    amount: '£1,000'
+    amount: '£8,750'
   },
   {
     payee_name: 'Oakley and Carter Farm',
     town: 'Aston',
     county_council: 'Oxfordshire',
     part_postcode: 'OX18',
-    amount: '£1,000'
+    amount: '£33,200'
   },
   {
     payee_name: 'R T Cartwell Dairy',
     town: 'Aston Rowant',
     county_council: 'Oxfordshire',
     part_postcode: 'OX9',
-    amount: '£1,000'
+    amount: '£22,750'
   },
   {
     payee_name: 'T R Calderbank & Partners',
     town: 'Aston Tirrold',
     county_council: 'Oxfordshire',
     part_postcode: 'OX11',
-    amount: '£1,000'
+    amount: '£11,000'
   },
   {
     payee_name: 'T R Capford & Partners',
     town: 'Aston Upthorpe',
     county_council: 'Oxfordshire',
     part_postcode: 'OX11',
-    amount: '£1,000'
+    amount: '£5,670'
   },
   {
-    payee_name: 'TR Carter & Co',
+    payee_name: 'T R Carter & Co',
     town: 'Balscote',
     county_council: 'Oxfordshire',
     part_postcode: 'OX15',
-    amount: '£1,000'
+    amount: '£21,250'
   },
   {
     payee_name: 'TR Smith Farms',
     town: 'Bampton',
     county_council: 'Oxfordshire',
     part_postcode: 'OX18',
-    amount: '£1,000'
+    amount: '£31,350'
   },
   {
     payee_name: 'Carling Dairy',
     town: 'Banbury',
     county_council: 'Oxfordshire',
     part_postcode: 'OX16',
-    amount: '£1,000'
+    amount: '£7,230'
   },
   {
     payee_name: 'Carver Trent Farm',
     town: 'Barford St John',
     county_council: 'Oxfordshire',
     part_postcode: 'OX15',
-    amount: '£1,000'
+    amount: '£11,630'
   },
   {
     payee_name: 'Carlisle Dairy',
     town: 'Barford St Michael',
     county_council: 'Oxfordshire',
     part_postcode: 'OX15',
-    amount: '£1,000'
+    amount: '£2,430'
   },
   {
     payee_name: 'Carter & Carter Farm',
     town: 'Baynards Green',
     county_council: 'Oxfordshire',
     part_postcode: 'OX6',
-    amount: '£1,000'
+    amount: '£6,650'
   },
   {
     payee_name: 'Carter Right Ltd',
     town: 'Beckley',
     county_council: 'Oxfordshire',
     part_postcode: 'OX3',
-    amount: '£1,000'
+    amount: '£17,550'
   },
   {
     payee_name: 'Carter Pickford Farm',
     town: 'Begbroke',
     county_council: 'Oxfordshire',
     part_postcode: 'OX5',
-    amount: '£1,000'
+    amount: '£3,780'
   },
   {
     payee_name: 'Carter and Smith Dairy',
     town: 'Benson',
     county_council: 'Oxfordshire',
     part_postcode: 'OX10',
-    amount: '£1,000'
+    amount: '£19,750'
   },
   {
     payee_name: 'Steven Carter Ltd',
     town: 'Beringsfield',
     county_council: 'Oxfordshire',
     part_postcode: 'OX10',
-    amount: '£1,000'
+    amount: '£15,580'
   },
   {
     payee_name: 'Darren Carwright and Sons Ltd',
     town: 'Berrick Salome',
     county_council: 'Oxfordshire',
     part_postcode: 'OX10',
-    amount: '£1,000'
+    amount: '£6,600'
   },
   {
     payee_name: 'Carter Grain Ltd',
     town: 'Bessels Leigh',
     county_council: 'Oxfordshire',
     part_postcode: 'OX13',
-    amount: '£1,000'
+    amount: '£16,720'
   },
   {
-    payee_name: 'TL Carter Organic Farm',
+    payee_name: 'T L Carter Organic Farm',
     town: 'Bicester',
     county_council: 'Oxfordshire',
     part_postcode: 'OX25',
-    amount: '£1,000'
+    amount: '£23,700'
   },
   {
-    payee_name: 'TR Clayford Farm',
+    payee_name: 'T R Clayford Farm',
     town: 'Black Bourton',
     county_council: 'Oxfordshire',
     part_postcode: 'OX18',
-    amount: '£1,000'
+    amount: '£6,600'
   }
 
 ]
